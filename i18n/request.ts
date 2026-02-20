e79d34406237e7427ec12075b6db0ebac144bcfb
kkzakaria <kkzakaria@outlook.com>
2026-02-20 15:08:12 +0000

    Add next-intl getRequestConfig with multi-namespace message loading

---
+++ i18n/request.ts
@@ -0,0 +1,21 @@
+import { getRequestConfig } from "next-intl/server";
+import { hasLocale } from "next-intl";
+import { routing } from "./routing";
+
+export default getRequestConfig(async ({ requestLocale }) => {
+  const requested = await requestLocale;
+  const locale = hasLocale(routing.locales, requested)
+    ? requested
+    : routing.defaultLocale;
+
+  const [common, navigation, appbar] = await Promise.all([
+    import(`../messages/${locale}/common.json`).then((m) => m.default),
+    import(`../messages/${locale}/navigation.json`).then((m) => m.default),
+    import(`../messages/${locale}/appbar.json`).then((m) => m.default),
+  ]);
+
+  return {
+    locale,
+    messages: { common, navigation, appbar },
+  };
+});
